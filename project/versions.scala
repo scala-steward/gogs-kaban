483c2dfd25dbec9f15702824ed38d4438a1e676f
Scala Steward <scala_steward@virtuslab.com>
2025-09-17 17:27:54 +0000

    Update quill-jdbc-zio to 4.8.6

--- project/versions.scala
+++ project/versions.scala
@@ -10,7 +10,7 @@ object versions {
   val scalaJavaTimeVersion = "2.6.0"
   val jwtCoreVersion       = "10.0.1"
   val zioTestVersion       = "2.1.9"
-  val zioQuillVersion      = "4.8.4"
+  val zioQuillVersion      = "4.8.6"
   val mysqlVersion         = "8.0.33"
   val zioConfigVersion     = "4.0.2"
   val scalaShaVersion      = "2.0.2"
