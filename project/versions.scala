8316dda64e5f7b53f774fcbbae83aa411ab925a9
Scala Steward <scala_steward@virtuslab.com>
2025-09-17 17:28:31 +0000

    Update mysql-connector-java to 8.4.0

--- project/versions.scala
+++ project/versions.scala
@@ -11,7 +11,7 @@ object versions {
   val jwtCoreVersion       = "10.0.1"
   val zioTestVersion       = "2.1.9"
   val zioQuillVersion      = "4.8.4"
-  val mysqlVersion         = "8.0.33"
+  val mysqlVersion         = "8.4.0"
   val zioConfigVersion     = "4.0.2"
   val scalaShaVersion      = "2.0.2"
   val flywaydbVersion      = "10.19.0"
